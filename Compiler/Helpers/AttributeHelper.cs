d4d4ffa4f9d7092725e086ea672c4cba12c4d635
dubem <dubem@dubemenyekwe.com>
2016-02-01 16:42:29 -0600

    Simplified to use new attributes

---
+++ Compiler/Helpers/AttributeHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Compiler.Helpers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AttributeHelper
+    {
+        /// <summary>
+        /// Gets the attribute.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="enumValue">The enum value.</param>
+        /// <returns></returns>
+        public static T GetAttribute<T, E>(E enumValue) where T : Attribute
+        {
+            var enumType = typeof(E);
+            var attributeType = typeof(T);
+
+            return enumType
+                .GetMember(enumValue.ToString())
+                .FirstOrDefault()
+                .GetCustomAttributes(attributeType, false)
+                .Cast<T>()
+                .FirstOrDefault();
+        }
+    }
+}
--- Compiler/Models/KnownTokenTypes.cs
+++ Compiler/Models/KnownTokenTypes.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Compiler.Helpers;
+using Compiler.Models.Attributes;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Compiler.Models
@@ -33,6 +36,12 @@ namespace Compiler.Models
         /// The token type string value
         /// </summary>
         private static Dictionary<TokenType, string> TokenTypeStringValue;
+
+
+        /// <summary>
+        /// The known token types
+        /// </summary>
+        private static Dictionary<string, TokenType> _KnownTokenTypes;
 
         /// <summary>
         /// Gets the <see cref="System.String"/>(Lexeme) with the specified token type.
@@ -75,117 +84,25 @@ namespace Compiler.Models
         /// </summary>
         static KnownTokenTypes()
         {
-            TokenTypeStringValue = _KnownTokenTypes.ToDictionary(curr => curr.Value, curr => curr.Key);
-        }
+            var validateTokenType =  new Func<TokenType, KeyValuePair<string, TokenType>>((tokenType) =>
+            {
+                var attr = AttributeHelper.GetAttribute<TokenTypeMetadataAttribute, TokenType>(tokenType);
 
-        /// <summary>
-        /// The known token types
-        /// </summary>
-        private static Dictionary<string, TokenType> _KnownTokenTypes = new Dictionary<string, TokenType>
-        {
-            { "!", TokenType.BooleanNot },
-            { "!=", TokenType.NotEqual },
-            { "%", TokenType.Modulo },
-            { "%=", TokenType.ModuloEqual },
-            { "&", TokenType.LogicalAnd },
-            { "&&", TokenType.BooleanAnd },
-            { "&=", TokenType.LogicalAndEqual },
-            { "(", TokenType.OpenParen },
-            { ")", TokenType.CloseParen },
-            { "*", TokenType.Multiplication },
-            { "*=", TokenType.MultiplicationEqual },
-            { "+", TokenType.Plus },
-            { "++", TokenType.PlusPlus },
-            { "+=", TokenType.PlusEqual },
-            { ",", TokenType.Comma },
-            { "-", TokenType.Minus },
-            { "--", TokenType.MinusMinus },
-            { "-=", TokenType.MinusEqual },
-            { ".", TokenType.Dot },
-            { "/", TokenType.Divide },
-            { "/=", TokenType.DivideEqual },
-            { ":", TokenType.Colon },
-            { ";", TokenType.Semicolon },
-            { "<", TokenType.LessThan },
-            { "<<", TokenType.BitwiseLeftShift },
-            { "<<=", TokenType.BitwiseLeftShiftEqual },
-            { "<=", TokenType.LessThanOrEqual },
-            { "=", TokenType.Assignment },
-            { "==", TokenType.BooleanEqual },
-            { ">", TokenType.GreaterThan },
-            { ">=", TokenType.GreaterThanOrEqual },
-            { ">>", TokenType.BitwiseRightShift },
-            { ">>=", TokenType.BitwiseRightShiftEqual },
-            { ">>>", TokenType.UnsignedLeftShift },
-            { ">>>=", TokenType.UnsignedLeftShiftEqual },
-            { "?", TokenType.QuestionMark },
-            { "[", TokenType.OpenSquareBracket },
-            { "\"", TokenType.DoubleQuote },
-            { "]", TokenType.CloseSquareBracket },
-            { "^", TokenType.LogicalExclusiveOr },
-            { "^=", TokenType.LogicalExclusiveOrEqual },
-            { "{", TokenType.OpenCurlyBrace },
-            { "|", TokenType.LogicalOr },
-            { "|=", TokenType.LogicalOrEqual },
-            { "||", TokenType.BooleanOr },
-            { "}", TokenType.CloseCurlyBrace },
-            { "~", TokenType.LogicalNot },
-            { "abstract", TokenType.Abstract },
-            { "assert", TokenType.Assert },
-            { "boolean", TokenType.Boolean },
-            { "break", TokenType.Break },
-            { "byte", TokenType.Byte },
-            { "case", TokenType.Case },
-            { "catch", TokenType.Catch },
-            { "char", TokenType.Char },
-            { "class", TokenType.Class },
-            { "const", TokenType.Const },
-            { "continue", TokenType.Continue },
-            { "default", TokenType.Default },
-            { "do", TokenType.Do },
-            { "double", TokenType.Double },
-            { "else", TokenType.Else },
-            { "enum", TokenType.Enum },
-            { "extends", TokenType.Extends },
-            { "false", TokenType.False },
-            { "final", TokenType.Final },
-            { "finally", TokenType.Finally },
-            { "float", TokenType.Float },
-            { "for", TokenType.For },
-            { "goto", TokenType.Goto },
-            { "if", TokenType.If },
-            { "implements", TokenType.Implements },
-            { "import", TokenType.Import },
-            { "instanceof", TokenType.Instanceof },
-            { "int", TokenType.Int },
-            { "interface", TokenType.Interface },
-            { "length", TokenType.Length },
-            { "long", TokenType.Long },
-            { "main", TokenType.Main },
-            { "native", TokenType.Native },
-            { "new", TokenType.New },
-            { "package", TokenType.Package },
-            { "private", TokenType.Private },
-            { "protected", TokenType.Protected },
-            { "public", TokenType.Public },
-            { "return", TokenType.Return },
-            { "short", TokenType.Short },
-            { "static", TokenType.Static },
-            { "strictfp", TokenType.Strictfp },
-            { "super", TokenType.Super },
-            { "switch", TokenType.Switch },
-            { "synchronized", TokenType.Synchronized },
-            { "this", TokenType.This },
-            { "throw", TokenType.Throw },
-            { "throws", TokenType.Throws },
-            { "transient", TokenType.Transient },
-            { "true", TokenType.True },
-            { "try", TokenType.Try },
-            { "void", TokenType.Void },
-            { "volatile", TokenType.Volatile },
-            { "while", TokenType.While },
-            { "String", TokenType.String },
-            { "System.out.println", TokenType.SystemOutPrintln },
-        };
-    }
+                if (string.IsNullOrWhiteSpace(attr.Lexeme))
+                {
+                    return default(KeyValuePair<string, TokenType>);
+                }
+
+                return  new KeyValuePair<string, TokenType>(attr.Lexeme, tokenType);
+            });
+
+            var tokenTypesWithValidLexemes = Enum.GetValues(typeof(TokenType))
+                .Cast<TokenType>()
+                .Select(validateTokenType)
+                .Where(v => !v.Equals(default(KeyValuePair<string, TokenType>)));
+
+            TokenTypeStringValue = tokenTypesWithValidLexemes.ToDictionary(curr => curr.Value, curr => curr.Key);
+            _KnownTokenTypes = tokenTypesWithValidLexemes.ToDictionary(curr => curr.Key, curr => curr.Value);
+        }
+   }
 }
