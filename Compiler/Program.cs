8f0a195d6924089698ed5d11b5381f14478aa1a8
dubem <dubem@dubemenyekwe.com>
2016-02-21 14:33:48 -0600

    Move compilation logic to new Class
    NOTE: if the token is null there will be a null pointer exception when
    traversing the tokens. It's up to the caller to check for null.
    
    I'm not doing so because when I  try accessing a property the
    NullPointerException is thrown.

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,7 +1,5 @@
-﻿using Compiler.Models;
-using Compiler.Services;
+﻿using Compiler.Services;
 using System;
-using System.IO;
 
 namespace Compiler
 {
@@ -9,84 +7,17 @@ namespace Compiler
     {
         public static void Main(string[] args)
         {
-            var normalColor = Console.ForegroundColor;
-            var errorColor = ConsoleColor.Red;
-            var infoColor = ConsoleColor.Yellow;
-
             if (args.Length != 1)
             {
                 Console.WriteLine("Invalid usage - Usage EXECUTABLE JAVA_FILE");
                 return;
             }
 
-            try
-            {
-                var streamReader = new StreamReader(args[0] + "");
-                var lexAnalyzer = new LexicalAnalyzerService(streamReader);
-                var count = 1L;
-                var maxCount = 20;
-
-                Console.WriteLine(Token.PRINT_HEADER);
-
-                while (true)
-                {
-                    try
-                    {
-                        var token = lexAnalyzer.GetNextToken();
-
-                        if (token == null)
-                        {
-                            break;
-                        }
-
-                        if (count % maxCount == 0)
-                        {
-                            Console.WriteLine($"\nPage #{count / maxCount}\nPlease press enter to continue ... ");
-                            Console.ReadLine();
-                        }
-
-                        if (token.HasError)
-                        {
-                            Print(token, errorColor);
-                        }
-                        else if (token.Type == TokenType.Unknown)
-                        {
-                            Print(token, infoColor);
-                        }
-                        else
-                        {
-                            Print(token, normalColor);
-                        }
-
-                        count++;
-
-                        if (token.Type == TokenType.EndOfFile)
-                        {
-                            break;
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Print("Oops, there seems to be something wrong.\n\n", errorColor);
-                Print(ex.Message, errorColor);
-            }
+            var compiler = new CompilerService();
+            compiler.Compile(args[0]);
 
             Console.WriteLine($"\n\nAll done.\nPlease press enter to continue ... ");
             Console.ReadLine();
-        }
-
-        private static void Print(object obj, ConsoleColor color)
-        {
-            var cc = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            Console.WriteLine(obj);
-            Console.ForegroundColor = cc;
         }
     }
 }
---
+++ Compiler/Services/CompilerService.cs
@@ -0,0 +1,74 @@
+﻿using Compiler.Models;
+using System;
+using System.IO;
+
+namespace Compiler.Services
+{
+    public class CompilerService
+    {
+        private static ConsoleColor NormalColor = Console.ForegroundColor;
+        private static ConsoleColor ErrorColor = ConsoleColor.Red;
+        private static ConsoleColor InfoColor = ConsoleColor.Yellow;
+
+        public void Compile(string fileName)
+        {
+            try
+            {
+                var streamReader = new StreamReader(fileName);
+                var lexAnalyzer = new LexicalAnalyzerService(streamReader);
+            }
+            catch (Exception ex)
+            {
+                Print("Oops, there seems to be something wrong.\n\n", ErrorColor);
+                Print(ex.Message, ErrorColor);
+            }
+        }
+
+        private void TraverseAllTokens(LexicalAnalyzerService lexAnalyzer)
+        {
+            var count = 1L;
+            var maxCount = 20;
+
+            Console.WriteLine(Token.PRINT_HEADER);
+
+            while (true)
+            {
+                var token = lexAnalyzer.GetNextToken();
+
+                if (count % maxCount == 0)
+                {
+                    Console.WriteLine($"\nPage #{count / maxCount}\nPlease press enter to continue ... ");
+                    Console.ReadLine();
+                }
+
+                if (token.HasError)
+                {
+                    Print(token, ErrorColor);
+                }
+                else if (token.Type == TokenType.Unknown)
+                {
+                    Print(token, InfoColor);
+                }
+                else
+                {
+                    Print(token, NormalColor);
+                }
+
+                count++;
+
+                if (token.Type == TokenType.EndOfFile)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void Print(object obj, ConsoleColor color)
+        {
+            var cc = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(obj);
+            Console.ForegroundColor = cc;
+        }
+    }
+}
