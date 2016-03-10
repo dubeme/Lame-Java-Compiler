55d2d28ead2894e279dc9471b2abdca4f5a20596
dubem <dubem@dubemenyekwe.com>
2016-03-10 12:18:03 -0600

    Implemented the hash function for getting the index

--- Compiler/Models/SymbolTable.cs
+++ Compiler/Models/SymbolTable.cs
@@ -1,11 +1,22 @@
-﻿namespace Compiler.Models
+﻿using Compiler.Models.Table;
+using System.Collections.Generic;
+
+namespace Compiler.Models
 {
     public class SymbolTable
     {
-        private const int TABLE_SIZE = 211;
+        private const int PRIME_TABLE_SIZE = 211;
+        private readonly LinkedListNode<IEntry>[] _Table;
+
+        public SymbolTable()
+        {
+            _Table = new LinkedListNode<IEntry>[PRIME_TABLE_SIZE];
+        }
 
         public void Insert(Token token, int depth)
         {
+            var index = (int)Hash(token.Lexeme) % PRIME_TABLE_SIZE;
+            
         }
 
         public void WriteTable(int depth)
@@ -16,8 +27,33 @@
         {
         }
 
-        public void InitTable()
+        public IEntry Lookup(string lexeme)
         {
+            return null;
+        }
+
+        /// <summary>
+        /// Hashes the specified lexeme, using the hashpjw from P.J Weinberger's C compiler.
+        /// </summary>
+        /// <param name="lexeme">The lexeme.</param>
+        /// <returns>The hash of the lexeme</returns>
+        private uint Hash(string lexeme)
+        {
+            uint hash = 0, temp;
+
+            foreach (var ch in lexeme)
+            {
+                hash = (hash << 4) + ch;
+                temp = hash & 0xF0000000;
+
+                if (temp != 0)
+                {
+                    hash = hash ^ (temp >> 24);
+                    hash = hash ^ temp;
+                }
+            }
+
+            return hash;
         }
     }
 }
