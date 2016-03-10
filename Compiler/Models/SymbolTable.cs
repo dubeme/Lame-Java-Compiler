42ae439c7fa9916e2bfe2d0ce491a8ecaecb9176
dubem <dubem@dubemenyekwe.com>
2016-03-10 04:30:55 -0600

    Added symbol table barebone

---
+++ Compiler/Models/SymbolTable.cs
@@ -0,0 +1,23 @@
+﻿namespace Compiler.Models
+{
+    public class SymbolTable
+    {
+        private const int TABLE_SIZE = 211;
+
+        public void Insert(Token token, int depth)
+        {
+        }
+
+        public void WriteTable(int depth)
+        {
+        }
+
+        public void DeleteDepth(int depth)
+        {
+        }
+
+        public void InitTable()
+        {
+        }
+    }
+}
