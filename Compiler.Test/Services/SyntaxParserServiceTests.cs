10c46d47fdf5db9e196bb9288a5916f1ee2af9e4
dubem <dubem@dubemenyekwe.com>
2016-03-25 20:13:49 -0500

    Improved test

--- Compiler.Test/Services/SyntaxParserServiceTests.cs
+++ Compiler.Test/Services/SyntaxParserServiceTests.cs
@@ -1,105 +1,178 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Compiler.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Compiler.Models;
+﻿using Compiler.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Compiler.Services.Tests
 {
     [TestClass()]
     public class SyntaxParserServiceTests
     {
+        private const string justMain = @"
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
 
-        const string testString1= @"final class Main
-        {public static void main(String[] args){}}";
-
-        const string testString2 = @"class one {} class two{}
-        final class Main
-        {
-            public static void main(String[] args)
-            {
+        private const string main1Class = @"
+            class tim {
             }
-        }";
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
 
-        const string testString3 = @"class One
-        {
-            int erer;
-            final int kl = 2;
-            public void func(){ return ;}
-        }
-        class Two
-        {
-            int a;
-            boolean b;
-        } final class Main {
-            public static void main(String[] args)
-            {
+        private const string main2Classes = @"
+            class tim {
             }
-        }";
+            class tom {
+            }
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
 
-        const string testString4 = @"class tim { int x, y, z;}
-        final class Main {public static void main(String[] args){}}";
+        private const string classWithFields = @"
+            class tim {
+                int x, y, z;
+            }
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
 
-        const string testString5 = @"class tim { 
-            int x, y, z; 
-            public void ass(int a, int b, int c){ return;}
-        }
-        final class Main {public static void main(String[] args){}}";
-        
+        private const string classWithFieldsAndMethod = @"
+            class tim {
+                int x, y, z;
+                public void ass(int a, int b, int c){
+                    return;
+                }
+            }
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
+
+        private const string classWithFieldsAndMethods = @"
+            class tim {
+                int x, y, z;
+                boolean a, b, c;
+                public void ass(){
+                    return;
+                }
+                public int kit(){
+                    return ;
+                }
+                public boolean dim(){
+                    return ;
+                }
+            }
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
+
+        private const string classWithFieldsMethodsParameters = @"
+            class tim {
+                int x, y, z;
+                boolean a, b, c;
+                public void ass(){
+                    return;
+                }
+                public int kit(int x, int y, boolean z){
+                    return ;
+                }
+                public boolean sum(float num, int ans){
+                    return ;
+                }
+                public float jamb(float num){
+                    return ;
+                }
+            }
+            final class Main {
+                public static void main(String[] args){
+                }
+            }";
+
+        private const string classWithConstant = @"
+            class two{
+                final boolean BOOL = false;
+                final int NUMBER = 1234567890;
+                final float PI = 3.14;
+            }
+            final class Main {
+                public static void main(String[] args) {
+                }
+            }";
+
         [TestMethod()]
         [TestCategory("Syntax Tree Parser")]
-        public void TestJustMainClass()
+        public void TestJustMain()
         {
-            var parser = CreateSyntaxParserService(testString1);
-
+            var parser = CreateSyntaxParserService(justMain);
             parser.Parse();
         }
 
         [TestMethod()]
         [TestCategory("Syntax Tree Parser")]
-        public void TestMainClassAndMoreClasses()
+        public void TestMain1Class()
         {
-            var parser = CreateSyntaxParserService(testString2);
-
+            var parser = CreateSyntaxParserService(main1Class);
             parser.Parse();
         }
 
         [TestMethod()]
         [TestCategory("Syntax Tree Parser")]
-        public void TestFieldsAndMethodsInClass()
+        public void TestMain2Classes()
         {
-            var parser = CreateSyntaxParserService(testString3);
-
+            var parser = CreateSyntaxParserService(main2Classes);
             parser.Parse();
         }
 
         [TestMethod()]
         [TestCategory("Syntax Tree Parser")]
-        public void TestMultipleFieldOnOneLine()
+        public void TestClassWithFields()
         {
-            var parser = CreateSyntaxParserService(testString4);
-
+            var parser = CreateSyntaxParserService(classWithFields);
             parser.Parse();
         }
 
         [TestMethod()]
         [TestCategory("Syntax Tree Parser")]
-        public void TestMultipleParameterForMemberFunction()
+        public void TestClassWithFieldsAndMethod()
         {
-            var parser = CreateSyntaxParserService(testString5);
+            var parser = CreateSyntaxParserService(classWithFieldsAndMethod);
+            parser.Parse();
+        }
 
+        [TestMethod()]
+        [TestCategory("Syntax Tree Parser")]
+        public void TestClassWithFieldsAndMethods()
+        {
+            var parser = CreateSyntaxParserService(classWithFieldsAndMethods);
+            parser.Parse();
+        }
+
+        [TestMethod()]
+        [TestCategory("Syntax Tree Parser")]
+        public void TestClassWithFieldsMethodsParameters()
+        {
+            var parser = CreateSyntaxParserService(classWithFieldsMethodsParameters);
+            parser.Parse();
+        }
+
+        [TestMethod()]
+        [TestCategory("Syntax Tree Parser")]
+        public void TestClassWithConstant()
+        {
+            var parser = CreateSyntaxParserService(classWithConstant);
             parser.Parse();
         }
 
         private SyntaxParserService CreateSyntaxParserService(string data)
         {
-
             var streamReader = LexicalAnalyzerServiceTests.CreateStreamReaderWith(data);
             var lexAnalyzer = new LexicalAnalyzerService(streamReader);
             var symbolTable = new SymbolTable();
+            symbolTable.Printer = (dump) => { };
 
             return new SyntaxParserService(lexAnalyzer, symbolTable);
         }
