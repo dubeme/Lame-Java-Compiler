6b970d9a531331a0da31b5c526c8a75bd9a62bcb
dubem <dubem@dubemenyekwe.com>
2016-04-17 14:56:10 -0500

    Simpler test file

--- Compiler.Test/test.files/classWithExpression.java
+++ Compiler.Test/test.files/classWithExpression.java
@@ -1,109 +1,35 @@
 class calculator {
     int x, y, z;
-    public void test1() {
-        int a,b,c;
-        a= 5;
-        b= 10;
-        c= a + b;
+
+    public void test1(){
+        int ans; 
+        ans = 3 * 5 + 8 / 9;
         return ;
     }
 
-    public int ss_ub(int c, float d){
-        // d = ss_ub();
-        c = 3 + 4*7 - - 6/8;
-        return -1 - -d;
-    }
-    
-    public int validMethod(int c, int d){
-        d = calculator.ss_ub(1,2);
-        return 1 - 5;
-    }
-    
-    public int inValidMethod(int c, int d){
-        // d = calculator.sss_ub();
-        return 1 - d;
-    }
-    
-    public int s_eub(int c, int d){
-        return 1 - d;
-    }
-    
-    public int s_ugb(int c, int d){
-        return 1;
-    }
-    
-    public int tester(int a, int b){
-        int ans; 
-        ans = a + b;
-        return -1;
-    }
-    
     public int sum(int a, int b){
-        int ans; 
+        int ans;
         ans = a + b;
+        a = a*4;
+        b = -b/a;
         return ans;
     }
-    
-    public int sub(int c, int d){
-        return c - d;
+
+    public int test2(int a){
+        return -a + 27;
     }
-    
-    public float quadratic(float a, float b, float c, float x) {
-        float ans;
-        ans = a*x*x + b*x + c;
         
+    public int test3(int a, int b){
+        int ans; 
+        ans = calculator.sum(a, b);
+        calculator.sum(a, b);
         return ans;
     }
-    
-    public void alot(int a, int b){
-        int sum,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z; 
-        boolean bool;
-        a= 5;
-        b= 10;
-        c= a + b;
-        sum = - - - 2;
-        /*sum = 3 - - - - - - 5;
-        sum = 3 + 4 * 2 / ( 1 - 5 )*(2)/2+(3);
-        sum = 3 + 4 * 2 / ( 1 - 5 )*2/2+3;
-        sum = 3 + 4 * 2 / 1 - 5*2/2+3;
-        sum = 1+2-3*4;
-        sum = a+b-c*d/5 - z*6;
-        sum = -2;
-        sum = -(9*7- - (4 + 9 * 6));
-        sum = 1+2-3*-4;
-        sum = 1+2- -3*-4;
-        sum = 1+-2-3*-4;
-        sum = -1+2-3*-4;
-        sum = !!!!!!!(bool);
-        sum = !!!!!(false);
-        sum = (2)+(3*4)- -(((a)));
-        sum = 3 * a + b / 5;
-        sum = a + b;*/
-        return ;
-    }
-    public void magic(int a, int b){
-        int ans; 
-        ans = 3 * a + b / 5;
-        return ;
-    }
-    public void undefined(int a, int b){
-        int ans;
-        ans = 3 * a + b / 5;
-        return ;
-    }
-    public void pemdas(int a, int b){
-        int ans;
-        ans = (2)+(3*4)-(((a)));
-        return ;
-    }
-    public void pemdas2(int a, int b){
-        int ans;
-        ans = !!!!!!!!!!true;
-        ans = !!!!!(false);
-        return ;
-    }
+
 }
+
 final class Main {
     public static void main(String[] args){
+        calculator.test2(1,2);
     }
 }
