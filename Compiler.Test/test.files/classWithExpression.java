56488a67cede7ac5599afe4fb39be898d4c5424e
dubem <dubem@dubemenyekwe.com>
2016-04-17 14:00:59 -0500

    added some tests

--- Compiler.Test/test.files/classWithExpression.java
+++ Compiler.Test/test.files/classWithExpression.java
@@ -1,8 +1,35 @@
 class calculator {
     int x, y, z;
+    public void test1() {
+        int a,b,c;
+        a= 5;
+        b= 10;
+        c= a + b;
+        return ;
+    }
+
+    public int ss_ub(int c, float d){
+        // d = ss_ub();
+        c = 3 + 4*7 - - 6/8;
+        return -1 - -d;
+    }
     
-    public int s_ub(int c, int d){
+    public int validMethod(int c, int d){
+        d = calculator.ss_ub(1,2);
+        return 1 - 5;
+    }
+    
+    public int inValidMethod(int c, int d){
+        // d = calculator.sss_ub();
         return 1 - d;
+    }
+    
+    public int s_eub(int c, int d){
+        return 1 - d;
+    }
+    
+    public int s_ugb(int c, int d){
+        return 1;
     }
     
     public int tester(int a, int b){
@@ -57,23 +84,23 @@ class calculator {
     public void magic(int a, int b){
         int ans; 
         ans = 3 * a + b / 5;
-        return ans;
+        return ;
     }
     public void undefined(int a, int b){
         int ans;
         ans = 3 * a + b / 5;
-        return ans;
+        return ;
     }
     public void pemdas(int a, int b){
         int ans;
-        ans = (2)+(3*4)-(((a));
-        return ans;
+        ans = (2)+(3*4)-(((a)));
+        return ;
     }
     public void pemdas2(int a, int b){
         int ans;
-        ans = !!!!!!!!!!2;
-        ans = !!!!(false);
-        return ans;
+        ans = !!!!!!!!!!true;
+        ans = !!!!!(false);
+        return ;
     }
 }
 final class Main {
